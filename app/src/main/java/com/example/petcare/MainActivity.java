99674fdeed16520da81029034329cc8e80f12abb
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-09-19 14:57:00 -0300

    Primer commit PETCare

---
+++ app/src/main/java/com/example/petcare/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.petcare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Referencias de los botones
+        Button btnEmpleado = findViewById(R.id.btnEmpleado);
+        Button btnCliente = findViewById(R.id.btnCliente);
+
+        // codigo para el botón de Empleado
+        btnEmpleado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirigir a la actividad de Empleado
+                Intent intent = new Intent(MainActivity.this, EmpleadoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // codigo para el botón de Cliente
+        btnCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirigir a la actividad de Cliente
+                Intent intent = new Intent(MainActivity.this, ClienteSeleccionadoActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
