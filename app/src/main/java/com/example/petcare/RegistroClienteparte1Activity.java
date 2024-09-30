ca626b5e384aebade118c626d4b00b75e689e586
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-09-30 17:10:10 -0300

    domingo commit PETCare

--- app/src/main/java/com/example/petcare/RegistroClienteparte1Activity.java
+++ app/src/main/java/com/example/petcare/RegistroClienteparte1Activity.java
@@ -1,6 +1,9 @@
 package com.example.petcare;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,25 @@ public class RegistroClienteparte1Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registro_clienteparte1);
+
+        // Ajustar ventanas para el modo Edge-to-Edge
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Inicializar el botón "Cliente1"
+        Button btnCliente1 = findViewById(R.id.button13);
+
+        // Listener para el botón "Cliente1"
+        btnCliente1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirigir a RegistroClienteparte2Activity
+                Intent intent = new Intent(RegistroClienteparte1Activity.this, RegistroClienteparte2Activity.class);
+                startActivity(intent);
+            }
         });
     }
 }
