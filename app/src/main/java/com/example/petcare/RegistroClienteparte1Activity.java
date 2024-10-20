f68ade248689740e8d4c396a6308748be78feae7
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-10-20 01:27:17 -0300

    a

--- app/src/main/java/com/example/petcare/RegistroClienteparte1Activity.java
+++ app/src/main/java/com/example/petcare/RegistroClienteparte1Activity.java
@@ -1,9 +1,12 @@
 package com.example.petcare;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class RegistroClienteparte1Activity extends AppCompatActivity {
 
+    private String[] clients = {"Cliente 1", "Cliente 2", "Cliente 3", "Cliente 4"};
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registro_clienteparte1);
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -26,14 +31,16 @@ public class RegistroClienteparte1Activity extends AppCompatActivity {
             return insets;
         });
 
+        // Configurar ListView
+        ListView listViewClients = findViewById(R.id.listViewClients);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, clients);
+        listViewClients.setAdapter(adapter);
 
-        Button btnCliente1 = findViewById(R.id.button13);
-
-
-        btnCliente1.setOnClickListener(new View.OnClickListener() {
+        // Manejar clic en los elementos de la lista
+        listViewClients.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                // manda a RegistroClienteparte2Activity
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Dependiendo de la posición, puedes hacer algo diferente
                 Intent intent = new Intent(RegistroClienteparte1Activity.this, RegistroClienteparte2Activity.class);
                 startActivity(intent);
             }
