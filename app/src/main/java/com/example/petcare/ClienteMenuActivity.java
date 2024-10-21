9418bb966d94608f6c335f15f64bbf849df1369d
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-10-21 15:25:16 -0300

    a

--- app/src/main/java/com/example/petcare/ClienteMenuActivity.java
+++ app/src/main/java/com/example/petcare/ClienteMenuActivity.java
@@ -2,60 +2,62 @@ package com.example.petcare;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class ClienteMenuActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class ClienteMenuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cliente_menu);
 
-        // Ajustar ventanas para el modo Edge-to-Edge
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-        Button btnSolicitarHorario = findViewById(R.id.button7);
-        Button btnRevisarFechaHorario = findViewById(R.id.button8);
-        Button btnOrdenarProducto = findViewById(R.id.button9);
+        if (savedInstanceState == null) {
+            navigationView.setCheckedItem(R.id.nav_request_opening_hours);
+        }
+    }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_request_opening_hours:
+                startActivity(new Intent(ClienteMenuActivity.this, SeleccionAtencionHoraActivity.class));
+                break;
+            case R.id.nav_check_date_operation_hours:
+                startActivity(new Intent(ClienteMenuActivity.this, RevisionHorarioAtencionActivity.class));
+                break;
+            case R.id.nav_order_product:
+                startActivity(new Intent(ClienteMenuActivity.this, OpcionOrdenarVeterinariaActivity.class));
+                break;
+        }
 
-        btnSolicitarHorario.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ClienteMenuActivity.this, SeleccionAtencionHoraActivity.class);
-                startActivity(intent);
-            }
-        });
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
-
-        btnRevisarFechaHorario.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ClienteMenuActivity.this, RevisionHorarioAtencionActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-        btnOrdenarProducto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ClienteMenuActivity.this, OpcionOrdenarVeterinariaActivity.class);
-                startActivity(intent);
-            }
-        });
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
