7da685d0d24e7610fbb05530cd8fd56d97650928
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-10-25 00:20:14 -0300

    a

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
 plugins {
     // No necesitas importar com.android.builder.files.classpathToRelativeFileSet aquí.
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.google.services) apply false
+
 }
 
 allprojects {
