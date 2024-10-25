44ad601c227b1520082ce748d6bcb61182a9832a
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-10-25 00:43:49 -0300

    a

--- app/src/main/java/com/example/petcare/ConfirmacionEliminacionProductoActivity.java
+++ app/src/main/java/com/example/petcare/ConfirmacionEliminacionProductoActivity.java
@@ -1,8 +1,8 @@
 package com.example.petcare;
 
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,12 +13,13 @@ public class ConfirmacionEliminacionProductoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.fragment_confirmacion_eliminacion_producto);
+
+        // Ajustar ventanas para el modo Edge-to-Edge
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+            return WindowInsetsCompat.CONSUMED; // Consume los insets
         });
     }
 }
