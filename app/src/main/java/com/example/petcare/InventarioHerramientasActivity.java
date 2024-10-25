bc0e69ba638ddb1d660e7c195b4ff5560d47c52c
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-10-25 00:49:59 -0300

    a

--- app/src/main/java/com/example/petcare/InventarioHerramientasActivity.java
+++ app/src/main/java/com/example/petcare/InventarioHerramientasActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -16,44 +15,31 @@ public class InventarioHerramientasActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.fragment_inventario_herramientas);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-
         Button btnAgregar = findViewById(R.id.btnAgregar);
         Button btnModificar = findViewById(R.id.btnModificar);
         Button btnEliminar = findViewById(R.id.btnEliminar);
 
-
-        btnAgregar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(InventarioHerramientasActivity.this, OpcionAgregarVeterinariaActivity.class);
-                startActivity(intent);
-            }
+        btnAgregar.setOnClickListener(v -> {
+            Intent intent = new Intent(InventarioHerramientasActivity.this, OpcionAgregarVeterinariaActivity.class);
+            startActivity(intent);
         });
 
-
-        btnModificar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(InventarioHerramientasActivity.this, OpcionModificarVeterinariaActivity.class);
-                startActivity(intent);
-            }
+        btnModificar.setOnClickListener(v -> {
+            Intent intent = new Intent(InventarioHerramientasActivity.this, OpcionModificarVeterinariaActivity.class);
+            startActivity(intent);
         });
 
-
-        btnEliminar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(InventarioHerramientasActivity.this, OpcionEliminarVeterinariaActivity.class);
-                startActivity(intent);
-            }
+        btnEliminar.setOnClickListener(v -> {
+            Intent intent = new Intent(InventarioHerramientasActivity.this, OpcionEliminarVeterinariaActivity.class);
+            startActivity(intent);
         });
     }
 }
--- app/src/main/java/com/example/petcare/InventarioMedicamentosActivity.java
+++ app/src/main/java/com/example/petcare/InventarioMedicamentosActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -16,44 +15,31 @@ public class InventarioMedicamentosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.fragment_inventario_medicamentos);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-
         Button btnAgregar = findViewById(R.id.btnAgregar);
         Button btnModificar = findViewById(R.id.btnModificar);
         Button btnEliminar = findViewById(R.id.btnEliminar);
 
-
-        btnAgregar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(InventarioMedicamentosActivity.this, OpcionAgregarVeterinariaActivity.class);
-                startActivity(intent);
-            }
+        btnAgregar.setOnClickListener(v -> {
+            Intent intent = new Intent(InventarioMedicamentosActivity.this, OpcionAgregarVeterinariaActivity.class);
+            startActivity(intent);
         });
 
-
-        btnModificar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(InventarioMedicamentosActivity.this, OpcionModificarVeterinariaActivity.class);
-                startActivity(intent);
-            }
+        btnModificar.setOnClickListener(v -> {
+            Intent intent = new Intent(InventarioMedicamentosActivity.this, OpcionModificarVeterinariaActivity.class);
+            startActivity(intent);
         });
 
-
-        btnEliminar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(InventarioMedicamentosActivity.this, OpcionEliminarVeterinariaActivity.class);
-                startActivity(intent);
-            }
+        btnEliminar.setOnClickListener(v -> {
+            Intent intent = new Intent(InventarioMedicamentosActivity.this, OpcionEliminarVeterinariaActivity.class);
+            startActivity(intent);
         });
     }
 }
